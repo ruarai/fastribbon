60bec3dcdf7c40d8efc3a9b2c187c3805ae474c3
Ruarai Tobin <ruarai.tobin@unimelb.edu.au>
2026-01-29 11:29:23 +1100

    use data.table implementation

--- R/stat_fastribbon.R
+++ R/stat_fastribbon.R
@@ -1,6 +1,3 @@
-require(ggplot2)
-require(ggdist)
-
 
 StatFastribbon <- ggproto(
   "StatFastribbon",
@@ -9,93 +6,65 @@ StatFastribbon <- ggproto(
   compute_panel = function(self, data, scales, .width = c(0.5, 0.8, 0.95), na.rm = FALSE, ...) {
     if (nrow(data) == 0) return(data.frame())
 
+    dt <- as.data.table(data)
+
     lower_probs <- (1 - .width) / 2
     upper_probs <- 1 - lower_probs
     probs <- sort(unique(c(0.5, lower_probs, upper_probs)))
 
-    xs <- unique(data$x)
+    setorder(dt, group, x)
+    dt[, segment_id := .GRP, by = .(group, x)]
 
-    data <- data |>
-      arrange(x) |>
-      group_by(group, x) |>
-      mutate(draw_id = row_number(),
-             x = match(x, xs)) |>
-      ungroup() |>
-      mutate(col_id = paste(group, x, sep = "___"))
+    dt[, draw_id := rowid(segment_id)]
 
-    poisoned_cols <- data |>
-      filter(is.na(y)) |>
-      pull(col_id) |>
-      unique()
+    meta_map <- unique(dt[, .(segment_id, group, x)])
 
-    wide_data <- data |>
-      pivot_wider(
-        id_cols = draw_id,
-        names_from = col_id,
-        values_from = y,
-        values_fill = NA
-      )
+    poisoned_segments <- integer(0)
+    if (!na.rm) {
+      poisoned_segments <- dt[is.na(y), unique(segment_id)]
+    }
 
-    mat <- as.matrix(select(wide_data, -draw_id))
+    wide_dt <- dcast(dt, draw_id ~ segment_id, value.var = "y", fill = NA)
 
-    col_names <- colnames(mat)
-    meta_split <- strsplit(col_names, "___")
+    mat <- as.matrix(wide_dt[, -1])
 
-    meta_df <- data.frame(
-      group = as.integer(sapply(meta_split, `[`, 1)),
-      x     = as.numeric(sapply(meta_split, `[`, 2))
-    )
-
-    # Always do na.rm here to ensure we get results for groups
-    # with fewer samples
     qs <- matrixStats::colQuantiles(mat, probs = probs, na.rm = TRUE)
 
-    # But post-hoc re-make NAs here instead
-    if (!na.rm && length(poisoned_cols) > 0) {
-      is_poisoned <- colnames(mat) %in% poisoned_cols
-      qs[is_poisoned, ] <- NA
+    if (length(poisoned_segments) > 0) {
+      qs[poisoned_segments, ] <- NA
     }
 
     median_idx <- match(0.5, probs)
     medians <- qs[, median_idx]
 
-    results <- list()
     ordered_widths <- sort(.width, decreasing = TRUE)
 
-    for (i in seq_along(ordered_widths)) {
-      w <- ordered_widths[i]
-
+    results <- lapply(ordered_widths, function(w) {
       lp <- (1 - w) / 2
       up <- 1 - lp
 
-      l_idx <- match(lp, probs)
-      u_idx <- match(up, probs)
-
-      res_df <- data.frame(
-        x = xs[meta_df$x],
-        group = meta_df$group,
+      data.frame(
+        x = meta_map$x,
+        group = meta_map$group,
         y = medians,
-        ymin = qs[, l_idx],
-        ymax = qs[, u_idx],
+        ymin = qs[, match(lp, probs)],
+        ymax = qs[, match(up, probs)],
         .width = w,
         level = factor(w),
         f = NA_real_,
         pdf = NA_real_,
         density = NA_real_
       )
-
-      results[[i]] <- res_df
-    }
+    })
 
     out <- do.call(rbind, results)
-
     out[order(out$.width, decreasing = TRUE), ]
   }
 )
 
 stat_fastribbon <- function(mapping = NULL, data = NULL, geom = "lineribbon",
-                                 position = "identity", na.rm = FALSE, show.legend = NA,
-                                 inherit.aes = TRUE, .width = c(0.50, 0.80, 0.95), ...) {
+                            position = "identity", na.rm = FALSE, show.legend = NA,
+                            inherit.aes = TRUE, .width = c(0.50, 0.80, 0.95), ...) {
   layer(
     stat = StatFastribbon, data = data, mapping = mapping, geom = geom,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
